b8c03ec86c56f33719c89265420f30cc3987c87e
arthsmn <arthsmn@proton.me>
2025-03-20 20:47:17 -0300

    cliflux: init at 1.8.0

---
+++ pkgs/by-name/cl/cliflux/package.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  openssl,
+  stdenv,
+  darwin,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "cliflux";
+  version = "1.8.0";
+
+  src = fetchFromGitHub {
+    owner = "spencerwi";
+    repo = "cliflux";
+    tag = "v${version}";
+    hash = "sha256-AGkinlN5Ng0LXau6U9Ft+yMIFMpbrbup3R3c3UlglEM=";
+  };
+
+  useFetchCargoVendor = true;
+
+  cargoHash = "sha256-3nNvPQMnYRZlhUab0MSf39vMNidpMLJh56JSjlsrYAg=";
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs =
+    [
+      openssl
+    ]
+    ++ lib.optionals stdenv.isDarwin [
+      darwin.apple_sdk.frameworks.Security
+      darwin.apple_sdk.frameworks.SystemConfiguration
+    ];
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "A terminal client for Miniflux RSS reader";
+    homepage = "https://github.com/spencerwi/cliflux";
+    changelog = "https://github.com/spencerwi/cliflux/blob/${src.rev}/CHANGELOG.md";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ arthsmn ];
+    mainProgram = "cliflux";
+  };
+}
